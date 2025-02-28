9a819ff89611fe3a06c3a2566e0a3742d10571a0
Ari Nugraha <nugrahaa878@gmail.com>
2025-03-01 06:14:31 +0700

    add blog

---
+++ src/app/blog/page.tsx
@@ -0,0 +1,13 @@
+import { NextPage } from 'next';
+
+const BlogPage: NextPage = () => {
+  return (
+    <div className="min-h-screen p-8">
+      <h1 className="text-2xl font-bold mb-4">Blog</h1>
+      <p>This is a blog page</p>
+      <p>This is content blog</p>
+    </div>
+  )
+}
+
+export default BlogPage;
