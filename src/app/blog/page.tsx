ff17ba6dffb5481cae2104898dd460a744ea6cc7
Ari Nugraha <nugrahaa878@gmail.com>
2025-03-01 06:24:40 +0700

    change flex

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -2,7 +2,7 @@ import { NextPage } from 'next';
 
 const BlogPage: NextPage = () => {
   return (
-    <div className="min-h-screen p-8">
+    <div className="min-h-screen p-8 flex">
       <h1 className="text-2xl font-bold mb-4">Blog</h1>
       <p>This is a blog page</p>
       <p>This is content blog</p>
