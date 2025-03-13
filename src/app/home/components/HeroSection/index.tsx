af742f2962addc7648635605edca0399984376cf
Ari Nugraha <nugrahaa878@gmail.com>
2025-03-13 10:08:02 +0700

    adjust preview package

--- src/app/home/components/HeroSection/index.tsx
+++ src/app/home/components/HeroSection/index.tsx
@@ -12,15 +12,17 @@ const HeroSection: FC = () => {
         <div className="absolute inset-0 bg-black/50"></div>
       </div>
 
-      <div className="relative w-full container mx-auto px-8 md:px-4 flex flex-col justify-center items-start z-10">
-        <h1 className="text-4xl md:text-5xl lg:text-6xl font-bold text-white mb-4 max-w-2xl abhaya-libre">
-          A Journey of Faith, <br />Guided with Excellence
-        </h1>
-        <p className="text-sm text-white/90 mb-8 md:mb-12 max-w-xl">
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur sed lorem a ipsum placerat feugiat. Pellentesque vel turpis mi. Vestibulum facilisis sit amet tellus ac pharetra. Etiam ac urna eu massa blandit ornare. Integer
-        </p>
+      <div className="relative w-full container mx-auto md:px-4 flex flex-col justify-center items-start z-10">
+        <div className='px-8'>
+          <h1 className="text-4xl md:text-5xl lg:text-6xl font-bold text-white mb-4 max-w-2xl abhaya-libre">
+            A Journey of Faith, <br />Guided with Excellence
+          </h1>
+          <p className="text-sm text-white/90 mb-8 md:mb-12 max-w-xl">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur sed lorem a ipsum placerat feugiat. Pellentesque vel turpis mi. Vestibulum facilisis sit amet tellus ac pharetra. Etiam ac urna eu massa blandit ornare. Integer
+          </p>
 
-        <ExploreButton />
+          <ExploreButton />
+        </div>
 
         <PreviewPackages />
       </div>
--- src/app/home/components/PreviewPackage/index.tsx
+++ src/app/home/components/PreviewPackage/index.tsx
@@ -5,26 +5,26 @@ import hajj from '../../../../../public/assets/icons/hajj.png'
 
 const PreviewPackages = () => {
   return (
-    <div className="flex justify-around w-full text-white mt-28 gap-10">
+    <div className="flex md:flex-row justify-around w-full text-white mt-28 gap-10 md:gap-10 overflow-x-auto px-4">
       <div className="flex flex-col items-center">
-        <div className="h-24 w-24 relative mb-4">
+        <div className="w-20 h-20 md:h-24 md:w-24 relative mb-4">
           <Image src={nabawi} alt="nabawi" fill className="object-contain" />
         </div>
-        <p className="text-md px-10">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna.</p>
+        <p className="text-sm text-center md:text-md px-2 md:px-10 w-40 md:w-auto">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna.</p>
       </div>
-      <div className="w-1 bg-white h-52"></div>
+      <div className="w-1 bg-white h-52 hidden md:block"></div>
       <div className="flex flex-col items-center">
-        <div className="h-24 w-24 relative mb-4">
+        <div className="w-20 h-20 md:h-24 md:w-24 relative mb-4">
           <Image src={kabah} alt="kabah" fill className="object-contain" />
         </div>
-        <p className="text-md px-10">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna.</p>
+        <p className="text-sm text-center md:text-md px-2 md:px-10 w-40 md:w-auto">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna.</p>
       </div>
-      <div className="w-1 bg-white h-52"></div>
+      <div className="w-1 bg-white h-52 hidden md:block"></div>
       <div className="flex flex-col items-center">
-        <div className="h-24 w-24 relative mb-4">
+        <div className="w-20 h-20 md:h-24 md:w-24 relative mb-4">
           <Image src={hajj} alt="hajj" fill className="object-contain" />
         </div>
-        <p className="text-md px-10">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna.</p>
+        <p className="text-sm text-center md:text-md px-2 md:px-10 w-40 md:w-auto">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna.</p>
       </div>
     </div>
   )
