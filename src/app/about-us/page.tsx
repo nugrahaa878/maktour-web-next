e85c65931bffb5fc3ac453b3ef3818e56845fb0e
Ari Nugraha <nugrahaa878@gmail.com>
2025-03-29 23:16:53 +0700

    adjust about us page

--- src/app/about-us/page.tsx
+++ src/app/about-us/page.tsx
@@ -3,11 +3,13 @@ import VisionMision from "./components/VisionMision";
 import MeetThePeople from "./components/MeetThePeople";
 import Navbar from "@/components/navbar";
 import Footer from "@/components/footer";
+import AboutMaktour from "./components/AboutMaktour";
 
 const AboutUs = () => {
   return (
     <main className="min-h-screen">
       <Navbar isWhiteBackground />
+      <AboutMaktour />
       <PlanWithUs />
       <VisionMision />
       <MeetThePeople />
