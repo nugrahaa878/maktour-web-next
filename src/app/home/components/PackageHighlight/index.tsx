7b641e48924d87e3be91f771a3ba0ff9b3c1bf00
Ari Nugraha <nugrahaa878@gmail.com>
2025-04-08 09:49:09 +0700

    fix desktop padding

--- src/app/home/components/PackageHighlight/index.tsx
+++ src/app/home/components/PackageHighlight/index.tsx
@@ -69,7 +69,7 @@ const PackageHighlights: FC = () => {
           </p>
         </div>
 
-        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8 px-7">
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8 px-7 md:px-0">
           {packages.map((pkg, index) => (
             <div
               key={index}
