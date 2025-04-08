00dff8de616443a48fd36e05664efde4e57c2efb
Ari Nugraha <nugrahaa878@gmail.com>
2025-04-08 08:34:08 +0700

    create basic package detail page

---
+++ src/app/packages/[slug]/page.tsx
@@ -0,0 +1,11 @@
+import Navbar from "@/components/navbar";
+
+const PackageDetails = () => {
+  return (
+    <main className="min-h-screen">
+      <Navbar isWhiteBackground />
+    </main>
+  )
+}
+
+export default PackageDetails;
