62c28a6d729792152718ffb44e9349f8f56ab72d
Ari Nugraha <nugrahaa878@gmail.com>
2025-04-11 08:07:09 +0700

    integrate API package

---
+++ src/app/home/components/PackageHighlight/PackageList.tsx
@@ -0,0 +1,52 @@
+'use client'
+
+import { useLanguage } from "@/context/LanguageContext";
+import { useGetPackage } from "@/hooks/useGetPackage"
+import Image from "next/image";
+import bgCard from '../../../../../public/assets/images/mekah-3.jpg';
+import Chip from "@/components/Chip";
+import BookingButton from "./BookingButton";
+
+const PackageList = () => {
+  const { language } = useLanguage();
+  const { data } = useGetPackage();
+
+  const listPackage = data?.data.filter((pkg) => pkg.locale === language) ?? [];
+
+  if (!listPackage?.length) {
+    return <div className="flex flex-col items-center py-10">
+      <p className="text-2xl font-semibold mb-5">Package Not Found</p>
+      <p className="text-gray-600">There are no Package right now</p>
+    </div>
+  }
+
+  return <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8 px-7 md:px-0">
+    {listPackage.map((pkg, index) => (
+      <div
+        key={index}
+        className="relative h-[500px] rounded-xl shadow-lg overflow-hidden transition-transform duration-300 hover:-translate-y-2"
+      >
+        <Image
+          src={bgCard}
+          alt={pkg.name}
+          fill
+          className="object-cover" />
+        <div className="absolute inset-0 bg-gradient-to-t from-black/80 to-transparent"></div>
+        <div className="absolute inset-0 p-6 flex flex-col justify-end">
+          <Chip label={pkg.classification} className='w-fit mb-2 -ml-1' variant='primary' />
+          <h3 className="text-xl font-bold text-white mb-2">{pkg.name}</h3>
+
+          <div className="space-y-2 mb-6">
+          </div>
+          <div className="flex items-center justify-between">
+            <span className="text-md text-white">{pkg.priceInIdr}</span>
+            <BookingButton />
+          </div>
+        </div>
+      </div>
+    ))}
+  </div>
+
+}
+
+export default PackageList;
--- src/app/home/components/PackageHighlight/index.tsx
+++ src/app/home/components/PackageHighlight/index.tsx
@@ -1,7 +1,6 @@
 import { FC } from 'react';
-import Image from 'next/image';
-import BookingButton from './BookingButton';
-import Chip from '@/components/Chip';
+import PackageList from './PackageList';
+import ExploreMoreButton from './ExploreMoreButton';
 
 interface PackageFeature {
   icon: string;
@@ -17,45 +16,6 @@ interface Package {
   features: PackageFeature[];
 }
 
-const packages: Package[] = [
-  {
-    type: 'Economy',
-    title: 'Premium Hajj Package',
-    image: '/assets/images/mekah-1.jpg',
-    description: 'Experience the ultimate Hajj journey with premium accommodations and dedicated services',
-    price: 'USD 44.500 - 49.500 / Person',
-    features: [
-      { icon: '🏨', text: '5-star hotel accommodations' },
-      { icon: '🍽️', text: 'Full-board meals' },
-      { icon: '👤', text: 'Dedicated guide' }
-    ]
-  },
-  {
-    type: 'Business',
-    title: 'Standard Umrah Package',
-    image: '/assets/images/mekah-2.jpg',
-    description: 'Complete your Umrah with our comprehensive and comfortable package',
-    price: 'USD 44.500 - 49.500 / Person',
-    features: [
-      { icon: '🏨', text: '4-star hotel accommodations' },
-      { icon: '✈️', text: 'Return flights included' },
-      { icon: '🚌', text: 'Ground transportation' }
-    ]
-  },
-  {
-    type: 'Economy',
-    title: 'Family Umrah Package',
-    image: '/assets/images/mekah-3.jpg',
-    description: 'Special package designed for families with children-friendly accommodations',
-    price: 'USD 44.500 - 49.500 / Person',
-    features: [
-      { icon: '👨‍👩‍👧‍👦', text: 'Family rooms available' },
-      { icon: '🍽️', text: 'Special meal options' },
-      { icon: '📱', text: '24/7 support service' }
-    ]
-  }
-];
-
 const PackageHighlights: FC = () => {
   return (
     <section className="py-16">
@@ -69,38 +29,9 @@ const PackageHighlights: FC = () => {
           </p>
         </div>
 
-        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8 px-7 md:px-0">
-          {packages.map((pkg, index) => (
-            <div
-              key={index}
-              className="relative h-[500px] rounded-xl shadow-lg overflow-hidden transition-transform duration-300 hover:-translate-y-2"
-            >
-              <Image
-                src={pkg.image}
-                alt={pkg.title}
-                fill
-                className="object-cover" />
-              <div className="absolute inset-0 bg-gradient-to-t from-black/80 to-transparent"></div>
-              <div className="absolute inset-0 p-6 flex flex-col justify-end">
-                <Chip label={pkg.type} className='w-fit mb-2 -ml-1' variant='primary' />
-                <h3 className="text-xl font-bold text-white mb-2">{pkg.title}</h3>
-                <p className="text-white/90 mb-4">{pkg.description}</p>
-                <div className="space-y-2 mb-6">
-                </div>
-                <div className="flex items-center justify-between">
-                  <span className="text-md text-white">{pkg.price}</span>
-                  <BookingButton />
-                </div>
-              </div>
-            </div>
-          ))}
-        </div>
+        <PackageList />
+        <ExploreMoreButton />
 
-        <div className="mt-12 text-center">
-          <button className="bg-[#FFC100] text-[#232323] px-8 py-3 rounded-2xl font-medium transition-all duration-200 cursor-pointer hover:bg-[#FFD54F] hover:scale-105 hover:shadow-lg">
-            Explore More
-          </button>
-        </div>
       </div>
     </section>
   );
